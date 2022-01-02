c5fd575d5eb4081c6a57957b67ed6a3fee4971a9
Grizzly <uibbs2@gmail.com>
2022-01-02 06:17:03 +0100

    Update bookmarklet.js
    Commented a known bug

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,6 +4,8 @@ javascript:(
     var ch='g_sr';  // your twitter username
     var og=new Date();   og.setFullYear(og.getFullYear()-qy);   // today, but one year ago
     var ie=new Date(og); ie.setDate(ie.getDate()-1);            // yesterday, but one year ago (needed to the search algorithm)
+    // known bugs: on Jan 1st will present og as Dec 32nd, ie is correct as Dec 31st...
+    // will try some workaround
     
     window.open('https://twitter.com/search?src=typed_query&q=(from%3A'+ch+')%20until%3A'+(og.getFullYear())+'-'+(og.getMonth()+1)+'-'+(og.getDate()+1)+'%20since%3A'+(ie.getFullYear())+'-'+(ie.getMonth()+1)+'-'+(ie.getDate()+1));
   })
